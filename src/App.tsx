a497f0d101bc66e5b17b6f84c4efdc4abb466b84
Rodrigo Carvalho <rodrigogoulart97@gmail.com>
2025-11-21 20:02:54 -0300

    feat: adding loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { PlanoDetalhesPage } from "./pages/PlanoDetalhesPage";
+import { Loading } from "./components/atoms";
+
+const HomePage = lazy(() => import("./pages/HomePage").then(module => ({ default: module.HomePage })));
+const PlanoDetalhesPage = lazy(() => import("./pages/PlanoDetalhesPage").then(module => ({ default: module.PlanoDetalhesPage })));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/plano/:id" element={<PlanoDetalhesPage />} />
-      </Routes>
+      <Suspense fallback={<Loading fullScreen />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/plano/:id" element={<PlanoDetalhesPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
